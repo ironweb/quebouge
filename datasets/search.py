dada1e0a9aa342366b0e8f5149c59feefbbbb283
Alexandre Bourget <alexandre.bourget@savoirfairelinux.com>
2012-02-22 20:22:07 -0500

    Tweak le script de search.py

--- datasets/search.py
+++ datasets/search.py
@@ -2,6 +2,7 @@
 # -=- encoding: utf-8 -=-
 
 from lxml import etree
+from pprint import pprint
 
 doc = etree.parse("LOISIR_PAYANT.XML")
 root = doc.getroot()
@@ -13,9 +14,19 @@ def node_to_dict(node):
         ret[el.tag] = el.text
     return ret
 
+liste_adresses = set()
+desc_act = set()
+desc = set()
 for activity in root.getchildren():
     d = node_to_dict(activity)
-    print d
+    liste_adresses.add(d['ADRESSE'])
+    desc_act.add(d['DESCRIPTION_ACT'][:10])
+    desc.add(d['DESCRIPTION'][:10])
+
+print pprint(liste_adresses)
+print pprint(desc_act)
+print pprint(desc)
+print len(liste_adresses), len(desc_act), len(desc)
 
 
     
