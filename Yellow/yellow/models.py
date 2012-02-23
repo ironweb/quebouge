e83a6bf6b250a06f337d4018e95a446212b21479
Alexandre Bourget <alexandre.bourget@savoirfairelinux.com>
2012-02-22 21:40:41 -0500

    Update des modèles.

--- Yellow/yellow/models.py
+++ Yellow/yellow/models.py
@@ -2,6 +2,10 @@ from sqlalchemy import (
     Column,
     Integer,
     Text,
+    Unicode,
+    UnicodeText,
+    ForeignKey,
+    DateTime,
     )
 
 from sqlalchemy.ext.declarative import declarative_base
@@ -11,18 +15,38 @@ from sqlalchemy.orm import (
     sessionmaker,
     )
 
+from geoalchemy import (
+    GeometryColumn,
+    Point)
+
+
 from zope.sqlalchemy import ZopeTransactionExtension
 
 DBSession = scoped_session(sessionmaker(extension=ZopeTransactionExtension()))
 Base = declarative_base()
 
-class MyModel(Base):
-    __tablename__ = 'models'
+class Category(Base):
+    __tablename__ = 'categories'
     id = Column(Integer, primary_key=True)
-    name = Column(Text, unique=True)
-    value = Column(Integer)
+    name = Column(Unicode(255), unique=True)
+    parent_id = Column(Integer, ForeignKey('categories.id'))
+    parent = relationship("Category")
 
-    def __init__(self, name, value):
-        self.name = name
-        self.value = value
+class Activity(Base):
+    __tablename__= "activities"
+    id = Column(Integer, primary_key=True)    
+    title = Column(Unicode(255))
+    category_id = Column(Integer, ForeignKey("categories.id"))
+    category = relationship(Category)
+    location = Column(Unicode(255))
+    position = GeometryColumn(Point(2))
 
+class Occurence(Base):
+    __tablename__= "activities"
+    id = Column(Integer, primary_key=True)    
+    title = Column(Unicode(255))
+    activity_id = Column(Integer, ForeignKey("activities.id"))
+    activity = relationship(Activity)
+    dtstart = Column(DateTime)
+    dtend = Column(DateTime)
+    
