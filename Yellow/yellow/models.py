398c6d12b5426e0357eef48ad317ed1b5226ee42
Jimmy Bourassa <jimmy.bourassa@hooktstudios.com>
2012-02-23 01:35:30 -0500

    activity wip. dégueu.

--- Yellow/yellow/models.py
+++ Yellow/yellow/models.py
@@ -45,6 +45,17 @@ class Activity(Base):
     position = GeometryColumn(Point(2))
 GeometryDDL(Activity.__table__)
 
+
+    @classmethod
+    def query_from_params(cls, params=None):
+        if params is None: params = {}
+        query = DBSession.query(cls)
+        if 'bb' in params:
+            query.filter(cls.position.within(bb_to_polyon(params['bb'])))
+
+        return query
+
+
 class Occurence(Base):
     __tablename__= "occurences"
     id = Column(Integer, primary_key=True)
@@ -53,4 +64,14 @@ class Occurence(Base):
     activity = relationship(Activity)
     dtstart = Column(DateTime)
     dtend = Column(DateTime)
-    
+
+
+
+def bb_to_polyon(bb_str):
+    x1, y1, x2, y2  = bb_str.split(',')
+    polygon_path = x1, y1, x2, y1, x2, y2, x1, y2
+    return "POINT(%s)" % ', '.join(polygon_path)
+
+def lat_lon_to_point(point_str):
+    return "POINT(%s, %s)" % point_str.slit(',')
+
